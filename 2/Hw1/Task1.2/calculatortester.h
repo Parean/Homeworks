ea9cabc209bc195d055b06af6e5a27b8e483ecdc
Parean <smirnov.d.p.1997@gmail.com>
2016-05-08 00:57:03 +0600

    Fixed bugs

--- 2/Hw1/Task1.2/calculatortester.h
+++ 2/Hw1/Task1.2/calculatortester.h
@@ -12,21 +12,55 @@ public:
 	explicit CalculatorTester(QObject *parent = 0) : QObject(parent) {}
 
 private:
-	Calculator *calculator;
+	Calculator *calculator = nullptr;
 
 private slots:
-	void init()
-	{
-		calculator = new Calculator(array, "5*5*(34+(234/3)*345/(16-7))\0");
-	}
 
 	void cleanup()
 	{
 		delete calculator;
 	}
 
-	void testCalculation()
+
+	void testOfNumber()
 	{
+		calculator = new Calculator(array, "7\0");
+		QVERIFY2(calculator->calculation() == 7, "Incorrect result");
+	}
+
+	void testOfAddition()
+	{
+		calculator = new Calculator(array, "6+5\0");
+		QVERIFY2(calculator->calculation() == 11, "Incorrect result");
+	}
+
+	void testOfSubtraction()
+	{
+		calculator = new Calculator(array, "6-5\0");
+		QVERIFY2(calculator->calculation() == 1, "Incorrect result");
+	}
+
+	void testOfMultiply()
+	{
+		calculator = new Calculator(array, "6*5\0");
+		QVERIFY2(calculator->calculation() == 30, "Incorrect result");
+	}
+
+	void testOfDivision()
+	{
+		calculator = new Calculator(array, "30/6\0");
+		QVERIFY2(calculator->calculation() == 5, "Incorrect result");
+	}
+
+	void testOfSimpleExpression()
+	{
+		calculator = new Calculator(array, "4*18-6/2+7\0");
+		QVERIFY2(calculator->calculation() == 76, "Incorrect result");
+	}
+
+	void testOfExpressionWithBrackets()
+	{
+		calculator = new Calculator(array, "5*5*(34+(234/3)*345/(16-7))\0");
 		QVERIFY2(calculator->calculation() == 75600, "Incorrect result");
 	}
 };
